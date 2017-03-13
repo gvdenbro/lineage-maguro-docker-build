a5c1388d4af107fc2eac15bcc86efa5619af261c
gvdenbro <gregory.vandenbroucke@gmail.com>
2017-03-13 23:10:41 +0100

    fixes

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,19 @@ FROM ubuntu:15.04
 
 MAINTAINER Gregory Vandenbroucke (gregory.vandenbroucke@gmail.com)
 
+RUN apt-get update && apt-get install -y \
+    bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev \
+    lib32readline-gplv2-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk2.8-dev \
+    libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev maven openjdk-7-jdk \
+    cron
+
+RUN curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/local/bin/repo && chmod a+x /usr/local/bin/repo
+
 ENV USER_NAME 'LineageOS GregFoobar'
 ENV USER_MAIL 'LineageOS-GregFoobar@docker.host'
+
+ENV DEVICE_LIST 'maguro'
+ENV BRANCH_NAME 'cm-13.0'
 
 ENV SRC_DIR /srv/src
 ENV CCACHE_DIR /srv/ccache
@@ -11,9 +22,9 @@ ENV ZIP_DIR /srv/zips
 #ENV LMANIFEST_DIR /srv/local_manifests
 
 ENV USE_CCACHE 1
-ENV DEBUG true
-ENV CLEAN_OUTDIR true
-ENV CLEAN_AFTER_BUILD true
+ENV DEBUG false
+ENV CLEAN_OUTDIR false
+ENV CLEAN_AFTER_BUILD false
 
 ENV CRONTAB_TIME '0 10 * * *'
 
@@ -21,12 +32,6 @@ VOLUME $SRC_DIR
 VOLUME $CCACHE_DIR
 VOLUME $ZIP_DIR
 #VOLUME $LMANIFEST_DIR
-
-RUN apt-get update && apt-get install -y \
-    bc bison build-essential curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev \
-    lib32readline-gplv2-dev lib32z1-dev libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk2.8-dev \
-    libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev maven openjdk-7-jdk \
-    cron
 
 RUN mkdir -p $SRC_DIR
 RUN mkdir -p $CCACHE_DIR
--- src/build.sh
+++ src/build.sh
@@ -10,62 +10,83 @@ if [ "$DEBUG" = true ]; then
   DEBUG_LOG=$DOCKER_LOG
 fi
 
-# cd to working directory
-cd $SRC_DIR
+if ! [ -z "$DEVICE_LIST" ]; then
 
-# If the source directory is empty
-if ! [ "$(ls -A $SRC_DIR)" ]; then
+  # cd to working directory
+  cd $SRC_DIR
+
+  # If the source directory is empty
+  if ! [ "$(ls -A $SRC_DIR)" ]; then
     # Initialize repository
     echo ">> [$(date)] Initializing repository" >> $DOCKER_LOG
-    yes | repo init -u git://github.com/lineageos/android.git -b cm-13.0 2>&1 >&$DEBUG_LOG
-fi
+    yes | repo init -u git://github.com/lineageos/android.git -b $BRANCH_NAME 2>&1 >&$DEBUG_LOG
+  fi
 
-# Copy local manifests to the appropriate folder in order take them into consideration
-echo ">> [$(date)] Copying '$LMANIFEST_DIR/*.xml' to '$SRC_DIR/.repo/local_manifests/'" >> $DOCKER_LOG
-cp $LMANIFEST_DIR/*.xml $SRC_DIR/.repo/local_manifests/ >&$DEBUG_LOG
+  # Copy local manifests to the appropriate folder in order take them into consideration
+  echo ">> [$(date)] Copying '$LMANIFEST_DIR/*.xml' to '$SRC_DIR/.repo/local_manifests/'" >> $DOCKER_LOG
+  cp $LMANIFEST_DIR/*.xml $SRC_DIR/.repo/local_manifests/ >&$DEBUG_LOG
 
-# Go to "vendor/cm" and reset it's current git status ( remove previous changes ) only if the directory exists
-if [ -d "vendor/cm" ]; then
+  # Go to "vendor/cm" and reset it's current git status ( remove previous changes ) only if the directory exists
+  if [ -d "vendor/cm" ]; then
     cd vendor/cm
     git reset --hard 2>&1 >&$DEBUG_LOG
     cd $SRC_DIR
-fi
+  fi
 
-# Sync the source code
-echo ">> [$(date)] Syncing repository" >> $DOCKER_LOG
-repo sync 2>&1 >&$DEBUG_LOG
+  # Sync the source code
+  echo ">> [$(date)] Syncing repository" >> $DOCKER_LOG
+  repo sync 2>&1 >&$DEBUG_LOG
 
-# If requested, clean the OUT dir in order to avoid clutter
-if [ "$CLEAN_OUTDIR" = true ]; then
+  # If requested, clean the OUT dir in order to avoid clutter
+  if [ "$CLEAN_OUTDIR" = true ]; then
     echo ">> [$(date)] Cleaning '$ZIP_DIR'" >> $DOCKER_LOG
     cd $ZIP_DIR
-    rm -rf *.zip
+    rm *
     cd $SRC_DIR
-fi
+  fi
 
-# Prepare the environment
-echo ">> [$(date)] Preparing build environment" >> $DOCKER_LOG
-source build/envsetup.sh 2>&1 >&$DEBUG_LOG
+  # Prepare the environment
+  echo ">> [$(date)] Preparing build environment" >> $DOCKER_LOG
+  source build/envsetup.sh 2>&1 >&$DEBUG_LOG
 
+  # Set a custom updater URI if a OTA URL is provided
+  if ! [ -z "$OTA_URL" ]; then
+    echo ">> [$(date)] Adding OTA URL '$OTA_URL' to build.prop" >> $DOCKER_LOG
+    sed -i "1s;^;PRODUCT_PROPERTY_OVERRIDES += cm.updater.uri=$OTA_URL\n\n;" vendor/cm/config/common.mk >&$DEBUG_LOG
+  fi
 
-# Start the build
-echo ">> [$(date)] Starting build for maguro" >> $DOCKER_LOG
-if brunch maguro 2>&1 >&$DEBUG_LOG; then
-    # Move produced ZIP files to the main OUT directory
-    echo ">> [$(date)] Moving build artifacts for maguro to '$ZIP_DIR'" >> $DOCKER_LOG
-    cd $SRC_DIR
-    find out/target/product/maguro -name '*UNOFFICIAL*.zip*' -exec mv {} $ZIP_DIR \; >&$DEBUG_LOG
-else
-    echo ">> [$(date)] Failed build for maguro" >> $DOCKER_LOG
-fi
-# Clean everything, in order to start fresh on next build
-if [ "$CLEAN_AFTER_BUILD" = true ]; then
-    echo ">> [$(date)] Cleaning build for maguro" >> $DOCKER_LOG
-    make clean 2>&1 >&$DEBUG_LOG
-fi
-echo ">> [$(date)] Finishing build for maguro" >> $DOCKER_LOG
+  # Cycle DEVICE_LIST environment variable, to know which one may be executed next
+  IFS=','
+  for codename in $DEVICE_LIST; do
+    if ! [ -z "$codename" ]; then
+      # Start the build
+      echo ">> [$(date)] Starting breakfast build for $codename" >> $DOCKER_LOG
+      if breakfast $codename 2>&1 >&$DEBUG_LOG; then
+        echo ">> [$(date)] finished breafast for $codename to '$ZIP_DIR'" >> $DOCKER_LOG
+        cd $SRC_DIR
+      else
+        echo ">> [$(date)] Failed build for $codename" >> $DOCKER_LOG
+      fi
+      echo ">> [$(date)] Starting build for $codename" >> $DOCKER_LOG
+      if brunch $codename 2>&1 >&$DEBUG_LOG; then
+        # Move produced ZIP files to the main OUT directory
+        echo ">> [$(date)] Moving build artifacts for $codename to '$ZIP_DIR'" >> $DOCKER_LOG
+        cd $SRC_DIR
+        find out/target/product/$codename -name '*UNOFFICIAL*.zip*' -exec mv {} $ZIP_DIR \; >&$DEBUG_LOG
+      else
+        echo ">> [$(date)] Failed build for $codename" >> $DOCKER_LOG
+      fi
+      # Clean everything, in order to start fresh on next build
+      if [ "$CLEAN_AFTER_BUILD" = true ]; then
+        echo ">> [$(date)] Cleaning build for $codename" >> $DOCKER_LOG
+        make clean 2>&1 >&$DEBUG_LOG
+      fi
+      echo ">> [$(date)] Finishing build for $codename" >> $DOCKER_LOG
+    fi
+  done
 
-# Clean the src directory if requested
-if [ "$CLEAN_SRCDIR" = true ]; then
+  # Clean the src directory if requested
+  if [ "$CLEAN_SRCDIR" = true ]; then
     rm -Rf "$SRC_DIR/*"
+  fi
 fi
